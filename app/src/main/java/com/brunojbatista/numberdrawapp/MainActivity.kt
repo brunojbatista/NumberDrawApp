fe29aab7ac0c138f301e84c0ad8aa6ddc7cb22e3
Bruno Batista <bjnb.projetos@gmail.com>
2025-04-24 15:12:10 -0300

    feat: create draw number app

---
+++ app/src/main/java/com/brunojbatista/numberdrawapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.brunojbatista.numberdrawapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun drawNumber(view: View) {
+        val valueTextView = findViewById<TextView>(R.id.value)
+        val value = Random().nextInt(10) + 1
+        valueTextView.text = value.toString()
+    }
+
+}
